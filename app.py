3e41d1eb4ecf50d11e46dc4fca983046db5d2566
VedhaSankar <vedhasankar26@gmail.com>
2022-02-24 12:15:41 +0530

    Updates for 12:15:41   24/02/22

---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import Flask, jsonify, request, session, render_template, make_response
+from functools import wraps
+import datetime
+import jwt
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'thisissecret'
+
+def check_for_token(func):
+
+    @wraps(func)
+    def wrapped(*args, **kwargs):
+        token = request.args.get('token')
+        if not token:
+            return jsonify({'message': 'Token is missing!'}), 403
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+        except:
+            return jsonify({'message': "Invalid token"}), 403
+        return func(*args, **kwargs)
+    return wrapped
+
+
+@app.route('/')
+def index():
+
+    if not session.get('logged_in'):
+        return render_template('index.html')
+
+    else:
+        return "Currently logged in"
+
+@app.route('/public')
+def public():
+    return "Anyone can see this"
+
+@app.route('/auth', methods=['GET','POST'])
+@check_for_token
+def auth():
+
+    return "You are authenticated"
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    if request.form['username'] and request.form['password'] == 'admin':
+        session['logged_in'] = True
+        token = jwt.encode ({
+            'user'  : request.form['username'],
+            'exp'   : datetime.datetime.utcnow() + datetime.timedelta(seconds=15)
+        },
+        app.config['SECRET_KEY']
+        )
+        return jsonify({'token': token})
+        
+    else:
+        return make_response('Could not verify!', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+if __name__ == '__main__':
+
+    app.run(debug = True, port = 8500)
